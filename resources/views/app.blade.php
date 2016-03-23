b3bfcfa8cda5a4bdc413531392ba8d87315c9c83
Mark Kogon <mlkogon231@gmail.com>
2016-03-23 11:35:08 -0700

    episode20 app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,11 @@
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 <div class = "test">
+
+@if(Session::has('flash_message'))
+	<div class = "alert alert-success">{{ Session::get('flash_message') }}</div>
+@endif
+
 @yield('content')
 </div>
 @yield('footer')
