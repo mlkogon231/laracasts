7246407af6244636807bb97b08470dad466cfa64
Mark Kogon <mlkogon231@gmail.com>
2016-03-24 15:11:34 -0700

    this is working now

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,10 @@
 <html>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/css/select2.min.css" rel="stylesheet" />
+
+
 <div class = "container">
 
 
@@ -10,13 +14,14 @@
 </div>
 
 
-<script src="//code.jquery.com/jquery-2.2.1.min.js"></script>
+<script src="//code.jquery.com/jquery.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
 <script>
 $('#flash-overlay-modal').modal();
 //  $('div.alert').not('.alert-important').delay(3000).slideUp(300);
 </script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/js/select2.min.js"></script>
 
 @yield('footer')
 </html>
